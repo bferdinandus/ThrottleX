53510c009f015de9efd861010a9d8a202b5ef4ac
Ben Ferdinandus <2flyfish@gmail.com>
2024-05-19 21:15:02 +0200

    Added statistics page

--- src/WebServer/Pages/Index.cshtml.cs
+++ src/WebServer/Pages/Index.cshtml.cs
@@ -1,19 +1,78 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace WebServer.Pages;
+using System.Diagnostics;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+    public string FreeDiskSpace { get; set; } = string.Empty;
+    public string FreeMemory { get; set; } = string.Empty;
+    public string CpuUsage { get; set; } = string.Empty;
+    public string CpuTemperature { get; set; } = string.Empty;
 
     public void OnGet()
     {
+        FreeDiskSpace = GetFreeDiskSpace();
+        FreeMemory = GetFreeMemory();
+        CpuUsage = GetCpuUsage();
+        CpuTemperature = GetCpuTemperature();
+    }
 
+    private string GetFreeDiskSpace()
+    {
+        var info = new ProcessStartInfo
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"df -h / | grep / | awk '{print $4}'\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+        var process = Process.Start(info);
+
+        return process?.StandardOutput.ReadToEnd().Trim() ?? "-";
+    }
+
+    private string GetFreeMemory()
+    {
+        var info = new ProcessStartInfo
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"free -h --mega| grep Mem | awk '{print $7}'\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+        var process = Process.Start(info);
+
+        return process?.StandardOutput.ReadToEnd().Trim() ?? "-";
+    }
+
+    private string GetCpuUsage()
+    {
+        var info = new ProcessStartInfo
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"LC_ALL=C top -bn1 | awk '/^%Cpu/ {print 100 - $8\\\"%\\\" }'\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+        var process = Process.Start(info);
+
+        return process?.StandardOutput.ReadToEnd().Trim() ?? "-";
+    }
+
+    private string GetCpuTemperature()
+    {
+        var info = new ProcessStartInfo
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"vcgencmd measure_temp | egrep -o '[0-9]*\\.[0-9]*'\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+        var process = Process.Start(info);
+
+        return $"{process?.StandardOutput.ReadToEnd().Trim()} °C";
     }
 }
