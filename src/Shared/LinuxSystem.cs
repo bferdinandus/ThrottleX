d21389d16874b1da7bf1e340975f9e09bffc0827
Ben Ferdinandus <2flyfish@gmail.com>
2024-05-20 20:54:28 +0200

    Housekeeping...

---
+++ src/Shared/LinuxSystem.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+
+namespace Shared;
+
+public static class LinuxSystem
+{
+    public static string GetFreeDiskSpace()
+    {
+        ProcessStartInfo info = new()
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"df -h / | grep / | awk '{print $4}'\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+        Process? process = Process.Start(info);
+
+        return process?.StandardOutput.ReadToEnd().Trim() ?? "-";
+    }
+
+    public static string GetFreeMemory()
+    {
+        ProcessStartInfo info = new()
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"free -h --mega| grep Mem | awk '{print $7}'\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+        Process? process = Process.Start(info);
+
+        return process?.StandardOutput.ReadToEnd().Trim() ?? "-";
+    }
+
+    public static string GetCpuUsage()
+    {
+        ProcessStartInfo info = new()
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"LC_ALL=C top -bn1 | awk '/^%Cpu/ {print 100 - $8\\\"%\\\" }'\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+        Process? process = Process.Start(info);
+
+        return process?.StandardOutput.ReadToEnd().Trim() ?? "-";
+    }
+
+    public static string GetCpuTemperature()
+    {
+        ProcessStartInfo info = new()
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"vcgencmd measure_temp | egrep -o '[0-9]*\\.[0-9]*'\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        };
+        Process? process = Process.Start(info);
+
+        return $"{process?.StandardOutput.ReadToEnd().Trim()} °C";
+    }
+}
--- src/WebServer/Pages/Index.cshtml.cs
+++ src/WebServer/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Diagnostics;
+
+using Shared;
+
+namespace WebServer.Pages;
 
 public class IndexModel : PageModel
 {
@@ -10,69 +13,9 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-        FreeDiskSpace = GetFreeDiskSpace();
-        FreeMemory = GetFreeMemory();
-        CpuUsage = GetCpuUsage();
-        CpuTemperature = GetCpuTemperature();
-    }
-
-    private string GetFreeDiskSpace()
-    {
-        var info = new ProcessStartInfo
-        {
-            FileName = "/bin/bash",
-            Arguments = "-c \"df -h / | grep / | awk '{print $4}'\"",
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true,
-        };
-        var process = Process.Start(info);
-
-        return process?.StandardOutput.ReadToEnd().Trim() ?? "-";
-    }
-
-    private string GetFreeMemory()
-    {
-        var info = new ProcessStartInfo
-        {
-            FileName = "/bin/bash",
-            Arguments = "-c \"free -h --mega| grep Mem | awk '{print $7}'\"",
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true,
-        };
-        var process = Process.Start(info);
-
-        return process?.StandardOutput.ReadToEnd().Trim() ?? "-";
-    }
-
-    private string GetCpuUsage()
-    {
-        var info = new ProcessStartInfo
-        {
-            FileName = "/bin/bash",
-            Arguments = "-c \"LC_ALL=C top -bn1 | awk '/^%Cpu/ {print 100 - $8\\\"%\\\" }'\"",
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true,
-        };
-        var process = Process.Start(info);
-
-        return process?.StandardOutput.ReadToEnd().Trim() ?? "-";
-    }
-
-    private string GetCpuTemperature()
-    {
-        var info = new ProcessStartInfo
-        {
-            FileName = "/bin/bash",
-            Arguments = "-c \"vcgencmd measure_temp | egrep -o '[0-9]*\\.[0-9]*'\"",
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true,
-        };
-        var process = Process.Start(info);
-
-        return $"{process?.StandardOutput.ReadToEnd().Trim()} °C";
+        FreeDiskSpace = LinuxSystem.GetFreeDiskSpace();
+        FreeMemory = LinuxSystem.GetFreeMemory();
+        CpuUsage = LinuxSystem.GetCpuUsage();
+        CpuTemperature = LinuxSystem.GetCpuTemperature();
     }
 }
