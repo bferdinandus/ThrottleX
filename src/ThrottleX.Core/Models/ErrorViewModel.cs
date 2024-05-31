c7ed1e42e165074bf090607402cabde365fe9b17
Ben Ferdinandus <2flyfish@gmail.com>
2024-05-31 21:56:38 +0200

    basic webserver running

---
+++ src/ThrottleX.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ThrottleX.Core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
