babb4da451f73b3f6c4b173ac1e94c3e13b51d6b
Ben Ferdinandus <2flyfish@gmail.com>
2024-05-31 21:31:48 +0200

    remove old files

--- src/WiThrottleServer/Program.cs
+++
@@ -1,9 +0,0 @@
-using WiThrottleServer;
-
-HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-builder.Logging.ClearProviders().AddConsole();
-
-IHost host = builder.Build();
-host.Run();
