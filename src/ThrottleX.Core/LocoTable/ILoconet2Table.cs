4dbbf997e19e69f323afd8b3fbeae32b2380b92c
Stefan Bormann <stefan.bormann@gmx.de>
2024-06-21 21:13:12 +0200

    Proposal for interface from LocoNet to loco table

---
+++ src/ThrottleX.Core/LocoTable/ILoconet2Table.cs
@@ -0,0 +1,17 @@
+﻿namespace ThrottleX.Core.LocoTable
+{
+    public interface ILoconet2Table
+    {
+        /// <summary>
+        /// Get a table row by its index.
+        /// </summary>
+        /// <param name="index">[0..Count-1]</param>
+        /// <returns>Reference to loco table row identified by its index.</returns>
+        ILoconet2Row this[int index] { get; }
+
+        /// <summary>
+        /// Number of entries in the table.
+        /// </summary>
+        int Count { get; }
+    }
+}
---
+++ src/ThrottleX.Core/LocoTable/LocoTableImpl.cs
@@ -0,0 +1,11 @@
+﻿namespace ThrottleX.Core.LocoTable
+{
+    public class LocoTableImpl : ILoconet2Table
+    {
+        private Dictionary<int, LocoRowImpl> _table = new();
+
+        public ILoconet2Row this[int index] => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+    }
+}
