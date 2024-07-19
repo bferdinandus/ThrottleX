27ec5e0fd1d4b493d7812a4c0440c096e7570dd9
Stefan Bormann <stefan.bormann@gmx.de>
2024-07-19 20:58:40 +0200

    OPC_LOCO_ADR

---
+++ src/Loconet/Msg/LocoAdr.cs
@@ -0,0 +1,28 @@
+﻿using Loconet.Msg.Accessor;
+
+namespace Loconet.Msg;
+
+internal class LocoAdr : FormatBase, ILoconetMessageFormat
+{
+    public static byte Opcode => 0xBF;
+
+    public static byte Length => 4;
+
+    public static bool IsVariableLength => false;
+
+    /// <summary>
+    /// Most significant bits of address, locope pretends "0" for this ;-)
+    /// </summary>
+    public Field7Bit AdrHigh;
+
+    /// <summary>
+    /// Least significant bits of address
+    /// </summary>
+    public Field7Bit Adr;
+
+    public LocoAdr()
+    {
+        AdrHigh = new(this, 1);
+        Adr = new(this, 2);
+    }
+}
