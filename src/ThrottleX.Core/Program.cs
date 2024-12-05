7c95f77ba7579be9fb9c8477915c762e97988695
Stefan Bormann <stefan.bormann@gmx.de>
2024-12-05 19:11:34 +0100

    serilog to fixed configuration and with thread ID logged

--- src/ThrottleX.Core/Program.cs
+++ src/ThrottleX.Core/Program.cs
@@ -1,10 +1,22 @@
 using Serilog;
 using ThrottleX.Core;
+using static Serilog.Events.LogEventLevel;
 
 CreateHostBuilder(args).Build().Run();
 return;
 
 static IHostBuilder CreateHostBuilder(string[] args) =>
     Host.CreateDefaultBuilder(args)
-        .UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration))
+        .UseSerilog(ConfigSerilog)
         .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+static void ConfigSerilog(HostBuilderContext context, LoggerConfiguration configuration)
+{
+    configuration.MinimumLevel.Verbose()
+                 .Enrich.WithThreadId()
+                 .WriteTo.Console(Debug)
+                 .WriteTo.File("Logs\\ThrottleX-.log",
+                               Verbose,
+                               rollingInterval: RollingInterval.Day,
+                               outputTemplate: "{Timestamp:DDD.HH:mm:ss.fff} [{Level:u3}] {ThreadId}: {Message:lj}{NewLine}{Exception}");
+}
