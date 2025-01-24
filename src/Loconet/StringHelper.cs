c211db436756bd1a86eb6b6f3c4819ade8c5425e
Stefan Bormann <stefan.bormann@gmx.de>
2025-01-24 22:01:36 +0100

    Add StringHelper.DisplayBits() and test

--- src/Loconet/StringHelper.cs
+++ src/Loconet/StringHelper.cs
@@ -26,4 +26,37 @@ public static class StringHelper
     {
         return BitConverter.ToString(bytes).Replace('-', ' ');
     }
+
+    public static string DisplayBits<T>(int value) where T : struct, Enum
+    {
+        return DisplayBits<T>(value, BoolByName);
+    }
+
+    public static string DisplayBits<T>(int value, Func<bool, string> bool2String) where T : struct, Enum
+    {
+        return string.Join(", ", EnumerateBits<T>(value, bool2String));
+    }
+
+    public static IEnumerable<string> EnumerateBits<T>(int value, Func<bool, string> bool2String) where T : struct, Enum
+    {
+        int allBitMasks = 0;
+
+        foreach (int bitMask in Enum.GetValuesAsUnderlyingType<T>())
+        {
+            allBitMasks |= bitMask;
+
+            var name = typeof(T).GetEnumName(bitMask);
+            var val = bool2String((value & bitMask) != 0);
+            yield return $"{name}({val})";
+        };
+
+        int invisibleBits = ~allBitMasks & value;
+        if (invisibleBits != 0)
+            yield return $"EXTRA-BITS(0x{invisibleBits:X})";
+    }
+
+    private static string BoolByName(bool b)
+    {
+        return b.ToString();
+    }
 }
---
+++ test/Unittests/Loconet/StringHelperTest.cs
@@ -0,0 +1,20 @@
+﻿using Loconet;
+
+namespace Unittests.Loconet;
+
+public class StringHelperTest
+{
+    private enum E
+    {
+        Zero = 1,
+        Two = 4,
+        Eight = 256
+    }
+
+    [Fact]
+    public void TestDisplayBits()
+    {
+        Assert.Equal("Zero(True), Two(False), Eight(True)", StringHelper.DisplayBits<E>(257));
+        Assert.Equal("Zero(False), Two(True), Eight(False), EXTRA-BITS(0x2)", StringHelper.DisplayBits<E>(6));
+    }
+}
